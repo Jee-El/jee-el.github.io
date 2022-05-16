99562e9983afd57fd381261aa7b8fadd56bfab4f
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-05-16 18:33:08 +0100

    Format files with Prettier

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const btnCancel = document.querySelector('.icon-cancel');
 const navList = document.querySelectorAll('.nav-links > *');
 const toggleBtn = document.querySelector('.icon-menu');
 toggleBtn.addEventListener('click', (e) => {
-    btnCancel.classList.add('active');
-    navLinksMobile.classList.add('active');
-})
+	btnCancel.classList.add('active');
+	navLinksMobile.classList.add('active');
+});
 
-navList.forEach((item) => item.addEventListener('click', () => {
-    btnCancel.classList.remove('active');
-    navLinksMobile.classList.remove('active');
-}))
-
+navList.forEach((item) =>
+	item.addEventListener('click', () => {
+		btnCancel.classList.remove('active');
+		navLinksMobile.classList.remove('active');
+	})
+);
